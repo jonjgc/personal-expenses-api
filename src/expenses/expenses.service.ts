098e2c75c6c66d638b841b0be8d884e34579f0a9
Jonatas <jonatasmc04@gmail.com>
2025-05-28 13:08:49 -0300

    implementação das operações CRUD no ExpensesService e ExpensesController

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateExpenseDto } from './dto/create-expense.dto';
+import { UpdateExpenseDto } from './dto/update-expense.dto';
 
 @Injectable()
-export class ExpensesService {}
+export class ExpensesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createExpenseDto: CreateExpenseDto) {
+    return this.prisma.expense.create({
+      data: createExpenseDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.expense.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.expense.findUnique({ where: { id } });
+  }
+
+  async update(id: string, updateExpenseDto: UpdateExpenseDto) {
+    return this.prisma.expense.update({
+      where: { id },
+      data: updateExpenseDto,
+    });
+  }
+
+  async remove(id: string) {
+    return this.prisma.expense.delete({ where: { id } });
+  }
+}
